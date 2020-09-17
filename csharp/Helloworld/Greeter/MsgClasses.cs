f8801993d80b7a7cb7c02f1dcc1f2825eb0fc242
KatFei <31780511+KatFei@users.noreply.github.com>
2020-09-18 01:30:55 +0300

    After updating pkgs

---
+++ csharp/Helloworld/Greeter/MsgClasses.cs
@@ -0,0 +1,66 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Grpc.Core;
+
+
+//namespace GrpcRssReader
+//{
+//    namespace MsgClasses
+//    {
+//        static class AuthRequest      //sealed
+//        {
+//            //Google.Protobuf.JsonFormatter;
+                
+//            static byte[] username;
+//            static byte[] password;
+//        }
+
+//        static class AuthReply
+//        {
+//            static byte[] checkresult;
+//            static byte[] authtoken;
+//        }
+
+//        static class SettingsRequest
+//        {
+//            static byte[] userid;
+//            static byte[] authtoken;
+//        }
+
+//        static class Settings
+//        {
+//            static byte[] userid;
+//            static byte[] field;
+//        }
+
+//        static class SettingsSavedReply
+//        {
+//            static byte[] count;
+//        }
+
+//        static class NewsRequest
+//        {
+//            static byte[] userid;
+//            static byte[] authtoken; //url
+//        }
+
+//        static class NewsReply
+//        {
+//            static byte[] subject;
+//            static byte[] summary;
+//            static byte[] url;
+//            static byte[] topic;
+//            static byte[] site;
+//            static byte[] date;
+//            static byte[] id; 
+//        }
+
+//        static class DeleteRequest
+//        {
+//            static byte[] checkresult;
+//            static byte[] authtoken;
+//        }
+
+//    }
+//}
